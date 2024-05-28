e77f7260f7079295621c84229c328678bd97e8d3
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-28 15:00:25 -0400

    Add routes, barebones actions

---
+++ src/routes.jsx
@@ -0,0 +1,22 @@
+import App from "./App";
+import Login from "./components/Login";
+import { loginAction, logoutAction } from "./actions";
+
+const routes = [
+    {
+        path: "/",
+        element: <App />,
+        children: []
+    },
+    {
+        path: "/login",
+        element: <Login />,
+        action: loginAction
+    },
+    {
+        path: "logout",
+        action: logoutAction
+    }
+];
+
+export default routes;
