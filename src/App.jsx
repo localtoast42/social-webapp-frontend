218ac2ea1cb77b6240bfff7ef7838c4463840148
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-28 16:12:08 -0400

    Add nav bar to root element

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css';
-import { Outlet } from "react-router-dom";
+import { Form, Outlet, NavLink, useLoaderData } from "react-router-dom";
 
 function App() {
+  const { user } = useLoaderData();
 
   return (
     <>
+      <nav>
+        <NavLink to="">Home</NavLink>
+        {user ? (
+          <Form method="post" action="logout">
+            <button>Sign out</button>
+          </Form>
+        ) : (
+          <NavLink to="login">Sign in</NavLink>
+        )}
+      </nav>
       <div>
         <Outlet />
       </div>
