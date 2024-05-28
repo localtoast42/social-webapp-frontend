baf9c8a8119d8d35562b37fccea01e7c1fcdfc63
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-28 14:59:30 -0400

    Add Login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,19 @@
+import { Form } from "react-router-dom";
+
+const Login = () => {
+
+    return (
+        <div>
+            <div>
+                <h2>Sign in</h2>
+                <Form method="post">
+                    <input type="text" name="username" id="username" placeholder="Username"/>
+                    <input type="password" name="password" id="password" placeholder="Password"/>
+                    <button type="submit">Sign in</button>
+                </Form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
