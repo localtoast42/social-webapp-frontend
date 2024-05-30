a9d48bb22b61a484b94c00cc442a5469eb1461d7
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-30 15:28:25 -0400

    Add UserContext to UserProfile component

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,10 +1,18 @@
+import { useContext } from "react";
 import { Outlet } from "react-router-dom";
+import { UserContext } from "../contexts/UserContext";
 
 function UserProfile() {
+  const user = useContext(UserContext);
 
   return (
     <div>
-      <Outlet />
+      <div>
+        <h1>Hello, {user.firstName}!</h1>
+      </div>
+      <div>
+        <Outlet />
+      </div>
     </div>
   )
 }
