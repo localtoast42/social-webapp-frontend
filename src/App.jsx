0d23d932316ec0a7ae6cd6269b0b92bd383e5e58
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-30 17:27:32 -0400

    Style nav bar elements

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,42 @@ function App() {
   const { user } = useLoaderData();
 
   return (
-    <>
-      <nav>
-        <NavLink to="home">Home</NavLink>
-        {user && <NavLink to={`/users/${user.id}`}>My Profile</NavLink>}
-        {user ? (
-          <Form method="post" action="logout">
-            <button>Sign out</button>
-          </Form>
-        ) : (
-          <NavLink to="login">Sign in</NavLink>
-        )}
+    <div className="flex min-h-full flex-col">
+      <nav className="flex items-center bg-gray-800">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="hidden sm:ml-6 sm:block">
+                <div className="flex space-x-4">
+                  <NavLink 
+                    to="home"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  >
+                    Home
+                  </NavLink>
+                  <NavLink 
+                    to={`/users/${user.id}`}
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  >
+                    My Profile
+                  </NavLink>
+                  <Form method="post" action="logout">
+                    <button className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
+                      Sign out
+                    </button>
+                  </Form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </nav>
       <div>
         <UserContext.Provider value={ user }>
           <Outlet />
         </UserContext.Provider>
       </div>
-    </>
+    </div>
   )
 }
 
