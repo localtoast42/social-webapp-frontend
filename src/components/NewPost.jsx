3d2a030c095e442e862750f639a3a077a8288cd1
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-31 15:37:29 -0400

    Add state to NewPost which toggles editor view

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,36 +1,82 @@
-import { Form } from "react-router-dom";
+import { useState } from "react";
+import { useFetcher } from "react-router-dom";
 
 export default function NewPost() {
+  const fetcher = useFetcher();
+  const [isEditable, setIsEditable] = useState(false);
+
+  function toggleEditable() {
+    setIsEditable(!isEditable);
+  }
 
   return (
     <div className="min-w-0 flex-1">
-      <Form className="relative">
-        <div className="overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
-          <textarea
-            rows={3}
-            id="text"
-            name="text"
-            className="block w-full resize-none border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+      {!isEditable && 
+        <button
+          type="button"
+          onClick={toggleEditable}
+          className="relative block w-full max-h-[138px] rounded-lg border-2 border-dashed border-gray-300 p-8 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          <svg
+            className="mx-auto h-12 w-12 text-gray-400"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
           >
-          </textarea>
-          <div className="py-2" aria-hidden="true">
-            <div className="py-px">
-              <div className="h-9" />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1.5}
+              d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+            />
+          </svg>
+          <span className="mt-2 block text-sm font-semibold text-gray-900">Create a new post</span>
+        </button>
+      }
+
+      {isEditable && 
+        <fetcher.Form 
+          method="post"
+          action="/posts/create"
+          onSubmit={toggleEditable}
+          className="relative"
+        >
+          <div className="overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
+            <textarea
+              rows={3}
+              id="text"
+              name="text"
+              defaultValue={''}
+              className="block w-full resize-none border-0 bg-transparent py-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+            >
+            </textarea>
+            <div className="py-2" aria-hidden="true">
+              <div className="py-px">
+                <div className="h-9" />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="absolute inset-x-0 bottom-0 flex justify-between py-2 pl-3 pr-2">
-          <div></div>
-          <div className="flex-shrink-0">
-            <button
-              type="submit"
-              className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Submit
-            </button>
+          <div className="absolute inset-x-0 bottom-0 flex justify-between py-2 pl-3 pr-2">
+            <div></div>
+            <div className="flex-shrink-0 flex space-x-2">
+              <button
+                type="submit"
+                className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Submit
+              </button>
+              <button
+                type="button"
+                onClick={toggleEditable}
+                className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Cancel
+              </button>
+            </div>
           </div>
-        </div>
-      </Form>
+        </fetcher.Form>
+      }
     </div>
   )
 }
