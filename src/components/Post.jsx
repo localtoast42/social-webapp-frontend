c397bda13cc5fa0d9beaba7425ba0f388340c7e0
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-31 13:22:23 -0400

    Styling Feed, Post components

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,21 +1,18 @@
-import Comment from "./Comment";
 import PropTypes from 'prop-types';
 
 function Post({ post }) {
-  const comments = post.comments;
 
   return (
-    <div>
-      <div>
-        <h2>{post.author}</h2>
-        <h2>{post.date}</h2>
-      </div>
-      <p>{post.text}</p>
-      <div>
-        {comments && comments.map(comment => <Comment 
-          key={comment.id}
-          comment={comment} />
-        )}
+    <div className="flex gap-x-4 py-5">
+      <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src="" alt="" />
+      <div className="flex-auto">
+        <div className="flex items-baseline justify-between gap-x-4">
+          <p className="text-sm font-semibold leading-6 text-gray-900">{post.author}</p>
+          <p className="flex-none text-xs text-gray-600">
+            <time dateTime={post.date}>{post.date}</time>
+          </p>
+        </div>
+        <p className="mt-1 line-clamp-2 text-sm leading-6 text-gray-600">{post.text}</p>
       </div>
     </div>
   )
