7eac0878fdfff883bd88c687d5379e5bbc993b71
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-31 13:22:07 -0400

    Styling UserProfile page

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -6,13 +6,13 @@ function UserProfile() {
   const user = useContext(UserContext);
 
   return (
-    <div>
-      <div>
-        <h1>Hello, {user.firstName}!</h1>
+    <div className="flex justify-center">
+      <div className="grow max-w-48 py-8">
+        <img className="h-24 w-24 flex-none rounded-full bg-gray-50" src="" alt="" />
+        <p className="text-xl font-bold">{user.firstName} {user.lastName}</p>
       </div>
-      <div>
-        <Outlet />
-      </div>
+      <Outlet />
+      <div className="grow max-w-48"></div>
     </div>
   )
 }
