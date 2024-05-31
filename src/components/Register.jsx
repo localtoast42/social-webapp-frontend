097345e3b2540069a7f0f413608b142a120f916a
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-05-31 16:20:29 -0400

    Style user register component, add error messages

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,21 +1,130 @@
-import { Form } from "react-router-dom";
+import { Form, useActionData } from "react-router-dom";
 
 const Register = () => {
+  const errors = useActionData();
+  console.log(errors);
 
   return (
-    <div>
-      <div>
-        <h2>Sign up</h2>
-        <Form method="post">
-          <input type="text" name="username" id="username" placeholder="Username"/>
-          <input type="password" name="password" id="password" placeholder="Password"/>
-          <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password"/>
-          <input type="text" name="firstName" id="firstName" placeholder="First Name"/>
-          <input type="text" name="lastName" id="lastName" placeholder="Last Name"/>
-          <button type="submit">Sign up</button>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign up
+        </h2>
+      </div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <Form method="post" className="space-y-6">
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label htmlFor="firstName" className="block text-sm font-medium leading-6 text-gray-900">
+                First Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors?.firstName && <span className="text-sm text-red-500">{errors.firstName.msg}</span>}
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label htmlFor="lastName" className="block text-sm font-medium leading-6 text-gray-900">
+                Last Name
+              </label>
+              <div className="mt-2">
+                <input 
+                  id="lastName" 
+                  name="lastName" 
+                  type="text"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+                {errors?.lastName && <span className="text-sm text-red-500">{errors.lastName.msg}</span>}
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+              Username
+            </label>
+            <div className="mt-2">
+              <input 
+                id="username"
+                name="username"
+                type="text" 
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              {errors?.username && <span className="text-sm text-red-500">{errors.username.msg}</span>}
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input 
+                id="password" 
+                name="password" 
+                type="password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              {errors?.password && <span className="text-sm text-red-500">{errors.password.msg}</span>}
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="confirm_password" className="block text-sm font-medium leading-6 text-gray-900">
+                Confirm Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input 
+                id="confirm_password" 
+                name="confirm_password" 
+                type="password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              {errors?.confirm_password && <span className="text-sm text-red-500">{errors.confirm_password.msg}</span>}
+            </div>
+          </div>
+
+          <div>
+            <button 
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Submit
+            </button>
+          </div>
         </Form>
       </div>
     </div>
+
+
+    // <div>
+    //   <div>
+    //     <h2>Sign up</h2>
+    //     <Form method="post">
+    //       <input type="text" name="username" id="username" placeholder="Username"/>
+    //       <input type="password" name="password" id="password" placeholder="Password"/>
+    //       <input type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password"/>
+    //       <input type="text" name="firstName" id="firstName" placeholder="First Name"/>
+    //       <input type="text" name="lastName" id="lastName" placeholder="Last Name"/>
+    //       <button type="submit">Sign up</button>
+    //     </Form>
+    //   </div>
+    // </div>
   );
 };
 
