3741d837c5c160db90f04a5f9e8d4715606570c5
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-05 16:14:10 -0400

    Add UserCard component for diplay in search list

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { Form, NavLink } from 'react-router-dom';
+
+function UserCard({ user }) {
+
+  return (
+    <li className="flex items-center justify-between gap-x-6 py-5">
+      <div className="flex min-w-0 gap-x-4">
+        <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={user.imageUrl} alt="" />
+        <div className="min-w-0 flex-auto">
+          <p className="text-sm font-semibold leading-6 text-gray-900">{user.fullName}</p>
+          <p className="mt-1 truncate text-xs leading-5 text-gray-500">{user.username}</p>
+        </div>
+      </div>
+      <Form method="post" action={`${user.url}/follow`}>
+        <button 
+          type="submit"
+          className="rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+        >
+          Follow
+        </button>
+      </Form>
+      <NavLink
+        to={user.url}
+        className="rounded-full bg-white px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+      >
+        View
+      </NavLink>
+    </li>
+  )
+}
+
+UserCard.propTypes = {
+  user: PropTypes.object.isRequired,
+}
+
+export default UserCard
