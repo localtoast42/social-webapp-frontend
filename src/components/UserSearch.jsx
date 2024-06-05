ca46da069194346d174e4ff32dcd915204ae3a4d
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-05 17:05:00 -0400

    Pass isFollowing prop to UserCards

--- src/components/UserSearch.jsx
+++ src/components/UserSearch.jsx
@@ -2,14 +2,15 @@ import UserCard from "./UserCard";
 import { useLoaderData } from "react-router-dom";
 
 function UserSearch() {
-  const { users } = useLoaderData();
+  const { users, following } = useLoaderData();
 
   return (
     <div className="mx-auto max-w-lg px-4 py-8 sm:px-6">
       <ul role="list" className="divide-y divide-gray-100">
-        {users.map(user => <UserCard
-          key={user.id}
-          user={user} />
+        {users.map(person => <UserCard
+          key={person.id}
+          user={person}
+          isFollowing={following.includes(person.id)} />
         )}
       </ul>
     </div>
