116805568252fb73f298705c3c0e5f5a9c4f7158
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-06 14:14:05 -0400

    Implement logic for liking post with like button

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,16 @@
 import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useFetcher } from 'react-router-dom';
 
 function Post({ post }) {
+  const fetcher = useFetcher();
+
+  const like = fetcher.formData 
+    ? fetcher.formData.get("like") === "true"
+    : post.isLiked;
+  
+  const numLikes = fetcher.formData 
+    ? (fetcher.formData.get("like") === "true" ? post.numLikes + 1 : post.numLikes - 1)
+    : post.numLikes;
 
   return (
     <div className="flex gap-x-4 py-2">
@@ -20,12 +29,17 @@ function Post({ post }) {
         <p className="mt-1 leading-6 text-gray-600">{post.text}</p>
         <div className="flex mt-2 gap-x-4">
           <div className="flex items-center gap-x-1">
-            <button>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-              </svg>
-            </button>
-            <p className="text-gray-900">100</p>
+            <fetcher.Form method="post" action={`/posts/${post.id}/like`}>
+              <button
+                name="like"
+                value={like ? "false" : "true"}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" fill={like ? "currentColor" : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                </svg>
+              </button>
+            </fetcher.Form>
+            <p className="text-gray-900">{numLikes}</p>
           </div>
           <div className="flex items-center gap-x-1">
             <button>
