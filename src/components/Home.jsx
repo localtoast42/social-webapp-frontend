41495feb7ca0a330e17446f541ddd656c0cc1983
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-07 15:12:36 -0400

    Add header to Home page view

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,16 @@ function Home() {
 
   return (
     <div className="flex justify-center">
-      <Outlet />
+      <div className="flex flex-col">
+        <header>
+          <div className="px-2 py-6">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   )
 }
