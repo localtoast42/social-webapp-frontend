0c2f927bc97e2460e7214d89bcb7b8602287c4d9
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-07 14:30:44 -0400

    Group sign out, profile links into one nav element

--- src/App.jsx
+++ src/App.jsx
@@ -6,43 +6,96 @@ import {
   NavLink, 
   useLoaderData 
 } from "react-router-dom";
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Transition,
+} from '@headlessui/react';
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 function App() {
   const { user } = useLoaderData();
 
   return (
     <div className="flex min-h-full flex-col">
-      <nav className="flex items-center bg-gray-800">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <nav className="flex justify-center items-center bg-gray-800">
+        <div className="grow max-w-xl">
           <div className="relative flex h-16 items-center justify-between">
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">
-                  <NavLink 
-                    to="home"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                  >
-                    Home
-                  </NavLink>
-                  <NavLink 
-                    to={`/users/${user.id}`}
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                  >
-                    My Profile
-                  </NavLink>
-                  <NavLink 
-                    to={`/users`}
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                  >
-                    Search Users
-                  </NavLink>
-                  <Form method="post" action="logout">
-                    <button className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">
-                      Sign out
-                    </button>
-                  </Form>
-                </div>
+              <div className="flex">
+                <NavLink
+                  to="home"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  to={`/users`}
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                >
+                  Search Users
+                </NavLink>
               </div>
+            </div>
+            <div className="flex">
+              <Menu as="div" className="relative ml-3">
+                <div>
+                  <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <span className="absolute -inset-1.5" />
+                    <span className="sr-only">Open user menu</span>
+                    <img
+                      className="h-8 w-8 rounded-full"
+                      src=""
+                      alt=""
+                    />
+                  </MenuButton>
+                </div>
+                <Transition
+                  enter="transition ease-out duration-100"
+                  enterFrom="transform opacity-0 scale-95"
+                  enterTo="transform opacity-100 scale-100"
+                  leave="transition ease-in duration-75"
+                  leaveFrom="transform opacity-100 scale-100"
+                  leaveTo="transform opacity-0 scale-95"
+                >
+                  <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <MenuItem>
+                      {({ focus }) => (
+                        <NavLink 
+                          to={`/users/${user.id}`}
+                          className={classNames(focus ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                        >
+                          Your Profile
+                        </NavLink>
+                      )}
+                    </MenuItem>
+                    <MenuItem>
+                      {({ focus }) => (
+                        <a
+                          href="#"
+                          className={classNames(focus ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                        >
+                          Settings
+                        </a>
+                      )}
+                    </MenuItem>
+                    <MenuItem>
+                      {({ focus }) => (
+                        <Form method="post" action="logout">
+                          <button className={classNames(focus ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                            Sign out
+                          </button>
+                        </Form>
+                      )}
+                    </MenuItem>
+                  </MenuItems>
+                </Transition>
+              </Menu>
             </div>
           </div>
         </div>
