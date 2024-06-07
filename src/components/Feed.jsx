36fcc02ddd3d796eba5f78c041da998c69c8f0e6
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-07 15:33:14 -0400

    Remove NewPost component from Feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,5 +1,4 @@
 import PostCard from "./PostCard";
-import NewPost from "./NewPost";
 import { useLoaderData } from "react-router-dom";
 
 function Feed() {
@@ -7,9 +6,6 @@ function Feed() {
 
   return (
     <div className="grow max-w-xl">
-      <div>
-        <NewPost></NewPost>
-      </div>
       <div className="mt-4 flex flex-col divide-y divide-gray-300">
         {posts.map(post => <PostCard
           key={post.id}
