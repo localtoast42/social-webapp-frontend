e0e2e5f2475efe0367b1d2534e1af511205545ee
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-07 15:22:23 -0400

    Add headers to profile, user search pages

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -9,7 +9,16 @@ function UserProfile() {
         <img className="h-24 w-24 flex-none rounded-full bg-gray-50" src="" alt="" />
         <p className="text-xl font-bold">{profile.firstName} {profile.lastName}</p>
       </div>
-      <Outlet />
+      <div className="grow max-w-xl">
+        <header>
+          <div className="px-2 py-6">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">{`${profile.firstName}'s Posts`}</h1>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
       <div className="grow max-w-48"></div>
     </div>
   )
--- src/components/UserSearch.jsx
+++ src/components/UserSearch.jsx
@@ -5,14 +5,23 @@ function UserSearch() {
   const { users, following } = useLoaderData();
 
   return (
-    <div className="mx-auto max-w-lg px-4 py-8 sm:px-6">
-      <ul role="list" className="divide-y divide-gray-100">
-        {users.map(person => <UserCard
-          key={person.id}
-          user={person}
-          isFollowing={following.includes(person.id)} />
-        )}
-      </ul>
+    <div className="flex justify-center">
+      <div className="grow max-w-xl">
+        <header>
+          <div className="px-2 py-6">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Search Users</h1>
+          </div>
+        </header>
+        <div className="grow max-w-xl px-4 sm:px-6">
+          <ul role="list" className="divide-y divide-gray-100">
+            {users.map(person => <UserCard
+              key={person.id}
+              user={person}
+              isFollowing={following.includes(person.id)} />
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
