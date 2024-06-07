b5afe622c54f41c07504e83edd5166902aedb7fd
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-07 14:32:54 -0400

    Add @ to username in UserSearch page

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -10,7 +10,7 @@ function UserCard({ user, isFollowing }) {
         <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={user.imageUrl} alt="" />
         <div className="min-w-0 flex-auto">
           <p className="font-semibold leading-6 text-gray-900">{user.fullName}</p>
-          <p className="mt-1 truncate leading-5 text-gray-500">{user.username}</p>
+          <p className="mt-1 truncate leading-5 text-gray-500">{`@${user.username}`}</p>
         </div>
       </div>
       <div className="flex gap-x-2">
