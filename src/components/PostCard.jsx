c193b20ab14aef527af4ee9435b45e4a481b5141
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-26 13:38:46 -0400

    Add default avatar image next to posts

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -15,12 +15,7 @@ function PostCard({ post }) {
 
   return (
     <div className="flex gap-x-4 py-2">
-      <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={defaultAvatar} alt="" />
-      <span className="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
-        <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-        </svg>
-      </span>
+      <img className="h-12 w-12 flex-none rounded-full bg-gray-100" src={defaultAvatar} alt="" />
       <div className="flex-auto">
         <div className="flex items-baseline justify-between gap-x-4">
           <NavLink 
