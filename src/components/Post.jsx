7ae6d730dd8e4eb91694d86312b33308fc647df7
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-27 12:20:05 -0400

    Add border between post and comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -77,13 +77,11 @@ function Post() {
           </div>
         </div>
         <div>
-          <div className="mt-4 flex flex-col divide-y divide-gray-300">
+          <div className="mt-4 flex flex-col divide-y divide-gray-300 border-t border-gray-300">
             {comments.map(comment => <Comment
               key={comment.id}
               comment={comment} />
             )}
-          </div>
-          <div className="mt-4">
             <NewComment postId={post.id}></NewComment>
           </div>
         </div>
