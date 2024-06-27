1bf75236e9dd5bd1b839558b569487635748205e
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-27 12:00:31 -0400

    Move NewPost into header element, adjust padding

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,14 @@ function Home() {
 
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col border border-gray-300">
-        <header>
-          <div className="px-3 py-3">
+      <div className="flex flex-col divide-y divide-gray-300 border border-gray-300">
+        <header className="py-3">
+          <div className="px-3">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
           </div>
+          <NewPost />
         </header>
         <main>
-          <NewPost />
           <Outlet />
         </main>
       </div>
