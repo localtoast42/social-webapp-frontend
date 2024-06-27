e15502a6ac9b36fe2b4688e263a9c6171b3b35fd
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-27 12:04:27 -0400

    Move NewPost element into header, add border

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -18,14 +18,14 @@ function UserProfile() {
         <p className="mt-2 text-xl leading-6 font-bold">{profile.fullName}</p>
         <p className="mt-1 truncate leading-5 text-gray-500">{`@${profile.username}`}</p>
       </div>
-      <div className="grow max-w-xl">
-        <header>
-          <div className="px-2 py-6">
+      <div className="grow max-w-xl divide-y divide-gray-300 border border-gray-300">
+        <header className="py-3">
+          <div className="px-3">
             <h1 className="text-3xl font-bold tracking-tight text-gray-900">{`${profile.firstName}'s Posts`}</h1>
           </div>
+          {userOwnedPage && <NewPost />}
         </header>
         <main>
-          {userOwnedPage && <NewPost />}
           <Outlet />
         </main>
       </div>
