b7c9ab6f33ec7424bbaa89bfe2e4208462e56d5c
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-28 14:40:30 -0400

    Update Home nav link to nav to recent feed

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
               <div className="flex space-x-4">
                 <NavLink
-                  to="/home"
+                  to="/home/recent"
                   end
                   className={({ isActive }) => 
                     classNames(isActive ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "rounded-md px-3 py-2 text-sm font-medium")
