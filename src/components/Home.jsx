9dfa669e21e22405e86c45aeb32c75cc9e2aae66
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-28 14:39:45 -0400

    Add tabs to nav between recent, following feeds

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,17 @@ import { UserContext } from "../contexts/UserContext";
 import { useContext } from "react";
 import { Outlet, NavLink } from "react-router-dom";
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
 function Home() {
   const user = useContext(UserContext);
+
+  const tabs = [
+    { name: 'Recent', href: 'recent' },
+    { name: 'Following', href: 'following' },
+  ]
 
   return (
     <div className="flex justify-center">
@@ -32,6 +41,29 @@ function Home() {
               </div>
             </div>
           }
+          <div>
+            <div className="hidden sm:block">
+              <div className="border-b border-gray-200">
+                <nav className="-mb-px px-3 flex space-x-8" aria-label="Tabs">
+                  {tabs.map((tab) => (
+                    <NavLink
+                      key={tab.name}
+                      to={tab.href}
+                      className={({ isActive }) => 
+                        classNames(
+                          isActive
+                          ? 'border-indigo-500 text-indigo-600'
+                          : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                        'whitespace-nowrap border-b-2 px-1 py-4 text-md font-medium',
+                        )}
+                    >
+                      {tab.name}
+                    </NavLink>
+                  ))}
+                </nav>
+              </div>
+            </div>
+          </div>
           <Outlet />
         </main>
       </div>
