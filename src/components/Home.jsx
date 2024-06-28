d53e9f1fc101cc8a95387af68e8a62de3f0ae92b
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-28 14:56:57 -0400

    Move tabs to top of Home view

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,36 +13,33 @@ function Home() {
 
   return (
     <div className="flex justify-center">
-      <div className="flex flex-col divide-y divide-gray-300 border border-gray-300">
-        <header className="py-6">
-          <div className="px-3">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+      <div className="grow max-w-xl flex flex-col divide-y divide-gray-300 border border-gray-300">
+        <header>
+          <div className="hidden sm:block">
+            <div className="border-b border-gray-200">
+              <nav className="-mb-px px-3 flex space-x-8" aria-label="Tabs">
+                {tabs.map((tab) => (
+                  <NavLink
+                    key={tab.name}
+                    to={tab.href}
+                    className={({ isActive }) => 
+                      classNames(
+                        isActive
+                        ? 'border-indigo-500 text-indigo-600'
+                        : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                      'whitespace-nowrap border-b-2 px-1 py-4 text-md font-medium',
+                      )}
+                  >
+                    {tab.name}
+                  </NavLink>
+                ))}
+              </nav>
+            </div>
           </div>
-          <NewPost />
         </header>
         <main>
-          <div>
-            <div className="hidden sm:block">
-              <div className="border-b border-gray-200">
-                <nav className="-mb-px px-3 flex space-x-8" aria-label="Tabs">
-                  {tabs.map((tab) => (
-                    <NavLink
-                      key={tab.name}
-                      to={tab.href}
-                      className={({ isActive }) => 
-                        classNames(
-                          isActive
-                          ? 'border-indigo-500 text-indigo-600'
-                          : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                        'whitespace-nowrap border-b-2 px-1 py-4 text-md font-medium',
-                        )}
-                    >
-                      {tab.name}
-                    </NavLink>
-                  ))}
-                </nav>
-              </div>
-            </div>
+          <div className="py-6 border-b border-gray-300">
+            <NewPost />
           </div>
           <Outlet />
         </main>
