11ad3563d1ce029e1b03c5fdb6a7f8803b255aa2
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-06-29 18:12:04 -0400

    Add post Edit, Delete buttons visible to authors

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,12 @@
 import Comment from './Comment';
 import NewComment from './NewComment';
+import { UserContext } from "../contexts/UserContext";
 import defaultAvatar from '../assets/defaultAvatar.svg';
+import { useContext } from "react";
 import { NavLink, useLoaderData, useFetcher } from 'react-router-dom';
 
 function Post() {
+  const user = useContext(UserContext);
   const { post } = useLoaderData();
   const comments = post.comments;
   const fetcher = useFetcher();
@@ -17,6 +20,8 @@ function Post() {
     : post.numLikes;
 
   const avatarUrl = post.author.imageUrl ? post.author.imageUrl : defaultAvatar;
+
+  const isPostAuthor = user.id === post.author.id;
 
   return (
     <div className="flex justify-center">
@@ -52,28 +57,44 @@ function Post() {
         </div>
         <div className="px-3">
           <p className="mt-1 leading-6 text-gray-600">{post.text}</p>
-          <div className="flex mt-2 gap-x-4">
-            <div className="flex items-center gap-x-1">
-              <fetcher.Form method="post" action={`/posts/${post.id}/like`} className="flex items-center">
-                <button
-                  name="like"
-                  value={like ? "false" : "true"}
-                >
-                  <svg xmlns="http://www.w3.org/2000/svg" fill={like ? "currentColor" : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+          <div className="mt-2 flex place-content-between">
+            <div className="flex gap-x-4">
+              <div className="flex items-center gap-x-1">
+                <fetcher.Form method="post" action={`/posts/${post.id}/like`} className="flex items-center">
+                  <button
+                    name="like"
+                    value={like ? "false" : "true"}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill={like ? "currentColor" : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                    </svg>
+                  </button>
+                </fetcher.Form>
+                <p className="text-gray-900">{numLikes}</p>
+              </div>
+              <div className="flex items-center gap-x-1">
+                <button>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
                   </svg>
                 </button>
-              </fetcher.Form>
-              <p className="text-gray-900">{numLikes}</p>
+                <p className="text-gray-900">{post.numComments}</p>
+              </div>
             </div>
-            <div className="flex items-center gap-x-1">
-              <button>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 0 1 1.037-.443 48.282 48.282 0 0 0 5.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
-                </svg>
-              </button>
-              <p className="text-gray-900">{post.numComments}</p>
-            </div>
+            {isPostAuthor && 
+              <div className="flex gap-x-4">
+                <button 
+                  className="flex px-3 py-1 justify-center items-center rounded-lg font-semibold leading-6 text-white bg-indigo-600 hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Edit
+                </button>
+                <button 
+                  className="flex px-3 py-1 justify-center items-center rounded-lg font-semibold leading-6 text-white bg-red-500 hover:bg-red-600"
+                >
+                  Delete
+                </button>
+              </div>
+            }
           </div>
         </div>
         <div>
