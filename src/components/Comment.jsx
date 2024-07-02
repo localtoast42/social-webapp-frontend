bc1b066ae9146708b3995dce864f13bd07e71a24
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-02 15:58:00 -0400

    Add delete button on Comments if you are author

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,15 +2,16 @@ import defaultAvatar from '../assets/defaultAvatar.svg';
 import PropTypes from 'prop-types';
 import { NavLink, useFetcher } from 'react-router-dom';
 
-function Comment({ comment }) {
-  const fetcher = useFetcher();
+function Comment({ comment, isCommentAuthor }) {
+  const likeFetcher = useFetcher();
+  const deleteFetcher = useFetcher();
 
-  const like = fetcher.formData 
-    ? fetcher.formData.get("like") === "true"
+  const like = likeFetcher.formData 
+    ? likeFetcher.formData.get("like") === "true"
     : comment.isLiked;
   
-  const numLikes = fetcher.formData 
-    ? (fetcher.formData.get("like") === "true" ? comment.numLikes + 1 : comment.numLikes - 1)
+  const numLikes = likeFetcher.formData 
+    ? (likeFetcher.formData.get("like") === "true" ? comment.numLikes + 1 : comment.numLikes - 1)
     : comment.numLikes;
 
   const avatarUrl = comment.author.imageUrl ? comment.author.imageUrl : defaultAvatar;
@@ -37,20 +38,36 @@ function Comment({ comment }) {
         <p className="mt-1 leading-6 text-gray-600">
           {comment.text}
         </p>
-        <div className="flex mt-2 gap-x-4">
-          <div className="flex items-center gap-x-1">
-            <fetcher.Form method="post" action={`comments/${comment.id}/like`} className="flex items-center">
-              <button
-                name="like"
-                value={like ? "false" : "true"}
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" fill={like ? "currentColor" : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                </svg>
-              </button>
-            </fetcher.Form>
-            <p className="text-gray-900">{numLikes}</p>
+        <div className="mt-2 flex place-content-between">
+          <div className="flex mt-2 gap-x-4">
+            <div className="flex items-center gap-x-1">
+              <likeFetcher.Form method="post" action={`comments/${comment.id}/like`} className="flex items-center">
+                <button
+                  name="like"
+                  value={like ? "false" : "true"}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill={like ? "currentColor" : "none"} viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+                  </svg>
+                </button>
+              </likeFetcher.Form>
+              <p className="text-gray-900">{numLikes}</p>
+            </div>
           </div>
+          {isCommentAuthor &&
+            <div className="flex gap-x-4">
+              <deleteFetcher.Form
+                method="post"
+                action={`/posts/${comment.post.id}/comments/${comment.id}/delete`}
+              >
+                <button
+                  className="flex px-3 py-1 justify-center items-center rounded-lg font-semibold leading-6 text-white bg-red-500 hover:bg-red-600"
+                >
+                  Delete
+                </button>
+              </deleteFetcher.Form>
+            </div>
+          }
         </div>
       </div>
     </div>
@@ -58,7 +75,8 @@ function Comment({ comment }) {
 }
 
 Comment.propTypes = {
-  comment: PropTypes.object.isRequired
+  comment: PropTypes.object.isRequired,
+  isCommentAuthor: PropTypes.bool.isRequired
 }
 
 export default Comment
