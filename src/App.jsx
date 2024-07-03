772102512e2e8076b48b4cba251495dcc952ae79
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-07-03 17:54:50 -0400

    Show Home nav item as active in sub-routes

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,6 @@ function App() {
               <div className="flex space-x-4">
                 <NavLink
                   to="/home"
-                  end
                   className={({ isActive }) => 
                     classNames(isActive ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white", "rounded-md px-3 py-2 text-sm font-medium")
                   }
